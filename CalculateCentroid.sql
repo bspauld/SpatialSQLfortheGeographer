7673e729630261d2e009ed3ff5e93f51df5ec20a
Ben Spaulding <bspauld@gmail.com>
2016-04-17 18:25:17 -0400

    Create CalculateCentroid.sql
    First!

---
+++ CalculateCentroid.sql
@@ -0,0 +1,21 @@
+/*
+  
+  Script:       CalculateCentroid.sql
+  Purpose:      Microsoft Transact SQL Script that will calculate the geometric centroid of the polygons in a table. 
+                Subquery will generate the centroid of each polygon record, and X and Y values can be derived in the
+                outer query.
+                
+                Data and tables in script based off tutorial information
+                from here: http://www.gisdoctor.com/site/2011/11/15/spatial-sql-geographer-part-1-spatial-sql/
+  
+  Instructions: Set proper table and field names and run the script.
+  Author: Ben Spaulding - 2016
+  
+*/
+
+select U.Region_Name, Centroid.STY as Longitude,
+Centroid.STX as Latidude
+ from
+(	 select geom.STCentroid() as Centroid, NAME as Region_Name
+	 from dbo.world_countries
+)U
