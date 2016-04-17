93214881d0d1bed7b34cd706ce8a371b4470ccfb
Ben Spaulding <bspauld@gmail.com>
2016-04-17 19:55:02 -0400

    Create UsingSpatialIndex.sql

---
+++ UsingSpatialIndex.sql
@@ -0,0 +1,21 @@
+/*
+  
+  Script:       UsingSpatialIndex.sql
+  Purpose:      Microsoft Transact SQL Script that provides a basic example of STIntersects(), a where statement, and
+                how to call a specific spatial index with the WITH statement. Calling the spatial index can 
+                greatly improve query speed.
+                
+                Data and tables in script based off tutorial information
+                from here: http://www.gisdoctor.com/site/2011/11/15/spatial-sql-geographer-part-1-spatial-sql/
+  
+  Instructions: Set proper table and field names and run the script.
+  Author: Ben Spaulding - 2016
+  
+*/
+
+select PP.*, SP.*
+from Populated_Places PP
+inner join States_Provinces SP with(Index(geom_sidx))
+on PP.geom.STIntersects(SP.geom) = 1
+where SP.NAME_0 = 'United States of America' 
+and SP.NAME_1 = 'Massachusetts'
