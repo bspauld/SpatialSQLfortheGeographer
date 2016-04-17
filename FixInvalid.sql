b7480c5e5bcfd4c3a037c89dfaa079b15654b95d
Ben Spaulding <bspauld@gmail.com>
2016-04-17 18:19:10 -0400

    Create FixInvalid.sql
    First commit

---
+++ FixInvalid.sql
@@ -0,0 +1,14 @@
+/*
+  
+  Script:       FixInvalid.sql
+  Purpose:      Microsoft Transact SQL Script that will fix any invalid geometries in described geomerty data.  
+                Data and tables in script based off tutorial information
+                from here: http://www.gisdoctor.com/site/2011/11/15/spatial-sql-geographer-part-1-spatial-sql/
+  
+  Instructions: Set proper table and field names and run the script.
+  Author: Ben Spaulding - 2016
+  
+*/
+
+update dbo.State_Provinces
+set geom = geometry::STGeomFromWKB(geom.MakeValid().STAsBinary(),geom.STSrid)
