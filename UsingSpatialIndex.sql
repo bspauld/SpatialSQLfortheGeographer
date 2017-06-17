00354cdd28328bfcdb9c1e2c81484e24e5f9e755
Ben Spaulding <bspauld@gmail.com>
2017-06-17 19:28:44 -0400

    Update UsingSpatialIndex.sql

--- UsingSpatialIndex.sql
+++ UsingSpatialIndex.sql
@@ -6,7 +6,7 @@
                 greatly improve query speed.
                 
                 Data and tables in script based off tutorial information
-                from here: http://www.gisdoctor.com/site/2011/11/15/spatial-sql-geographer-part-1-spatial-sql/
+                from here: http://www.benjaminspaulding.com/2011/11/15/spatial-sql-geographer-part-1-spatial-sql/
   
   Instructions: Set proper table and field names and run the script.
   Author: Ben Spaulding - 2016
