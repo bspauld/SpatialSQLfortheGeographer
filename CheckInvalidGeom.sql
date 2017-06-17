77b14e96bf4b64b3ea317a8b740477639554dfa9
Ben Spaulding <bspauld@gmail.com>
2017-06-17 19:16:12 -0400

    Update CheckInvalidGeom.sql

--- CheckInvalidGeom.sql
+++ CheckInvalidGeom.sql
@@ -3,7 +3,7 @@
   Script:       CheckInvalidGeom.sql
   Purpose:      Microsoft Transact SQL Script that will check data to see any invalid geometries exist in described geomerty data.  
                 Data and tables in script based off tutorial information
-                from here: http://www.gisdoctor.com/site/2011/11/15/spatial-sql-geographer-part-1-spatial-sql/
+                from here: http://www.benjaminspaulding.com/2011/11/15/spatial-sql-geographer-part-1-spatial-sql/
   
   Instructions: Set proper table and field names and run the script.
   Author: Ben Spaulding - 2016
